5d9a619c65dc5903c2eee636c283759395220813
Muhdsyarif <72724907+Muhdsyarif@users.noreply.github.com>
2021-04-13 06:15:19 +0800

    Add files via upload

---
+++ src/Smart_Phone.java
@@ -0,0 +1,74 @@
+
+public class Smart_Phone {
+	
+	String brand, model, colour, os;	// os = Operating System
+	double size, weight, price;
+	
+	// Create Setter Method
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+	
+	public void setOperatingSystem(String os) {
+		this.os = os;
+	}
+	
+	public void setSize(double size) {
+		this.size = size;
+	}
+	
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+	// Create Getter Method
+	public String getBrand() {
+		return this.brand;
+	}
+	
+	public String getModel() {
+		return this.model;
+	}
+	
+	public String getColour() {
+		return this.colour;
+	}
+	
+	public String getOperatingSystem() {
+		return this.os;
+	}
+	
+	public double getSize() {
+		return this.size;
+	}
+	
+	public double getWeight() {
+		return this.weight;
+	}
+	
+	public double getPrice() {
+		return this.price;
+	}
+	
+	public void TopPhone() {
+		System.out.println("Enjoy your online game with this new phone!!!");
+	}
+	
+	public void TopPhone2() {
+		System.out.println("The iPhone 11 is a smartphone designed, developed, and marketed by Apple Inc");
+	}
+
+}
